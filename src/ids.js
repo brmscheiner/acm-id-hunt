f7e875f9ec47bc517eb7d46e921c48ac5b646612
Ben Scheiner <brmscheiner@gmail.com>
2020-09-17 16:43:21 -0700

    Fetch ACM Ids

---
+++ src/ids.js
@@ -0,0 +1,24 @@
+
+
+const ids = [
+  { taskId: '2a2edd02-f544-4875-b90b-753548ac2f42', annotationId: '0001abd1-2faa-4407-b22b-9749437ae26e' },
+  { taskId: '906dd75e-4400-40a2-839a-f8a6a7304135', annotationId: '000445e7-3aa5-4e89-ae33-62295e311860' },
+  { taskId: '35e52d6f-864d-46d5-87e3-fbce19232445', annotationId: 'a8e8e450-048b-4a72-8ae6-51dcb0f26995' },
+  { taskId: 'a5dacec4-153b-407a-90e5-98f01b949a13', annotationId: '493366e4-5906-4096-a135-9c9753d130ed' },
+  { taskId: '50e6cd31-03d9-449d-823e-964f31000627', annotationId: '0008b188-8ba0-480e-915b-c129a3652aea' },
+  { taskId: '53233cbc-a0bf-4ec0-8e7f-8575c863e4c8', annotationId: '00120b0c-50f8-4fdd-b470-4872c07861a4' },
+  { taskId: 'b887927e-0c77-4914-a4c9-ef99dd98989a', annotationId: '001dbe7a-1124-40e7-84ed-a518ba247b94' },
+  { taskId: '2ce40f8e-d6d1-4cf5-9d68-dc0bdc3042d1', annotationId: '0025c1ef-fb06-4a41-acbf-208378538904' },
+  { taskId: '7867c23f-8166-4cc8-88b4-5553c897de0b', annotationId: '002eac92-3105-4487-a97a-f7d9348567bf' },
+  { taskId: 'c3912765-2214-4406-840e-c806651313fe', annotationId: '003ae3b6-27b3-48ab-9624-fad1d5ac8ce4' },
+  { taskId: '5fc3449d-eb53-40a1-9311-f51de2a478cb', annotationId: '003d81b0-8da1-41e3-b6bd-0dbd987dde1a' },
+  { taskId: '386a218b-ff43-4488-92e3-ad2f945a6940', annotationId: '003ef63b-e88b-42de-8b01-9531548c0398' },
+  { taskId: 'c4fda381-8d87-47a6-b80a-55adaefd7056', annotationId: '004145b5-2b22-447e-9c9c-35264214d7c9' },
+  { taskId: 'd0c56f11-bbb6-4506-bab0-3f1db8f4e4ed', annotationId: '0041cc3f-f697-490b-b517-2e08f0d1fde0' },
+  { taskId: '83d5b08f-1c20-4fc1-9b92-eb467d01e210', annotationId: '004791aa-6d5b-4008-883b-e3e0d51c091b' },
+  { taskId: '94d9ed0e-9a7b-49c8-94a3-4055e7b1a55a', annotationId: '005a3f62-a6ef-4831-ba2e-5467321daac8' },
+  { taskId: 'c4ea4b52-4fcb-4824-b3ee-66f609142278', annotationId: '005d22ee-a19c-4ac5-8830-720e5defa687' },
+  { taskId: '5721ff4e-f164-4333-9438-11dccb2eb36f', annotationId: '0064823b-154d-496a-a96b-d00ab9c4307c' },
+];
+
+export default ids;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import 'dotenv/config';
+import fs from 'fs';
+import axios from 'axios';
+import _ from 'lodash';
+import ids from './ids';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const sequentiallyFetchTaskData = async (idObjects) => {
+  for (const idObject of idObjects) {
+    await fetchTaskData(idObject)
+  }
+}
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
+const fetchTaskData = async (idObject) => {
+  try {
+    const response = await axios.request({
+      url: `https://www.flukebook.org/iaLogs.jsp?taskId=${idObject.taskId}`,
+      method: 'get',
+    });
+    const responseData = _.get(response, 'data', []);
+    const jobResultData = responseData.find(datum => _.get(datum, ['status', '_action']) === 'getJobResult');
+    if (!jobResultData) {
+      results.push({ ...idObject, success: false })
+    } else {
+      const cmDict = _.get(jobResultData, ['status', '_response', 'response', 'json_result', 'cm_dict']);
+      try {
+        const acmId = Object.keys(cmDict)[0];
+        results.push({ ...idObject, success: true, acmId });
+      } catch (error) {
+        results.push({ ...idObject, success: false })
+      }
+    }
+  } catch (fetchError) {
+    console.error('Error');
+    console.error(fetchError);
+  }
+}
 
-console.log(user);
+let results = [];
+sequentiallyFetchTaskData(ids).then(() => {
+  fs.writeFileSync('acmIds.json', JSON.stringify(results, null, 4));
+  console.log('Success')
+});
 
-console.log(process.env.SOME_ENV_VARIABLE);
+
